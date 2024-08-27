bfddfb75f3fd54d10bb968116e33885927666b0f
Fermin Rodriguez II <rdrgzfermin@gmail.com>
2024-08-27 00:51:21 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,15 +5,53 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let who = ["The dog", "My grandma", "The mailman", "My bird"];
-let action = ["ate", "peed", "crushed", "broke"];
-let what = ["my homework", "my phone", "the car"];
+let who = [
+  "My dog",
+  "My car",
+  "My sister",
+  "The neighbor",
+  "The Cat",
+  "The robber",
+  "My best friend",
+  "The store Clurk",
+  "The Mailperson"
+];
+let action = [
+  "bite",
+  "got a flat tire",
+  "crashed",
+  "done fucked up",
+  "ate",
+  "hurt",
+  "robbed",
+  "got the money wrong",
+  "went on vacation",
+  "pissed"
+];
+let what = [
+  "plant",
+  "tire",
+  "robber",
+  "on the floor",
+  "in the bush",
+  "my homework",
+  "my bird",
+  "the food",
+  "stole the car",
+  "my keys"
+];
 let when = [
-  "before the class",
-  "when I was sleeping",
-  "while I was exercising",
-  "during my lunch",
-  "while I was praying"
+  "yesterday.",
+  "this morning.",
+  "last night.",
+  "the weekend.",
+  "at the store.",
+  "driving.",
+  "before the class.",
+  "While i was sleeping.",
+  "during class.",
+  "during lunch.",
+  "while exercising."
 ];
 
 function getRandomNumber(min, max) {
@@ -30,7 +68,7 @@ function getRandom(anyArray) {
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
-  document.querySelector("#excuse").innerHTML =
+  document.querySelector("#weirdness").innerHTML =
     getRandom(who) +
     " " +
     getRandom(action) +
