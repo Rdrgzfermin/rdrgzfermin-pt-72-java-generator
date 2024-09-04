718a84889b00c3b2ed965d2c9f872d3f51a1da74
Fermin Rodriguez II <rdrgzfermin@gmail.com>
2024-09-04 23:58:38 +0000

    first

--- src/app.js
+++ src/app.js
@@ -58,3 +58,10 @@ window.onload = function() {
     " " +
     getRandom(when);
 };
+
+//const element = document.getElementById("myBtn");
+//element.addEventListener("click", myFunction);
+
+//function myFunction() {
+//document.getElementById("demo").innerHTML;
+//}
